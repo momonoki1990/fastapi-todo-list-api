2a4a50542ba6c38741193d878af6aa542831babb
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-12-05 17:31:53 +0900

    add settings for CORS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import task
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods="*"
+)
 
 app.include_router(task.router)

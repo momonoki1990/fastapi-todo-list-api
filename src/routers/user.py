1aef46d71ba79f877a0e08e93c260ab978d76d83
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-12-27 07:34:17 +0900

    add sample decode token function

--- src/routers/user.py
+++ src/routers/user.py
@@ -6,6 +6,12 @@ router = APIRouter(prefix="/users", tags=["user"])
 
 oauth2_schema: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl="/token")
 
-@router.get("/me", response_model=user_schema.User)
-async def get_user(token: str = Depends(oauth2_schema)):
+async def fake_decode_token(token: str):
     return user_schema.User(id=1, username="Naoya", email="nick@sample.com")
+
+async def get_current_user(token: str = Depends(oauth2_schema)):
+    return fake_decode_token(token)
+
+@router.get("/me", response_model=user_schema.User)
+async def get_user(current_user: user_schema.User = Depends(get_current_user)):
+    return current_user
